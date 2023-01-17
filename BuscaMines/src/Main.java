83b87b8fc54b6a8260ec1f11529b6a18873aaa91
Marc <marccorominas@inspladelestany.cat>
2023-01-17 16:04:21 +0100

    generar taulell

--- BuscaMines/src/Main.java
+++ BuscaMines/src/Main.java
@@ -11,9 +11,18 @@ public class Main {
         Scanner llegir = new Scanner(System.in);
         int alçada = Integer.parseInt(llegir.nextLine());
         int base = Integer.parseInt(llegir.nextLine());
-        int[][] taulell = new int[alçada][base];
+        alçada++;
+        alçada++;
+        base++;
+        base++;
+        String[][] taulell = new String[alçada][base];
         for(int i = 0; i<alçada; i++){
             for(int j = 0; j<base; j++){
+                if(i==0||i==(alçada-1)||j==0||j==(base-1)){
+                    taulell[i][j]="*";
+                }else{
+                    taulell[i][j]="0";
+                }
                 System.out.print(taulell[i][j]);
             }
             System.out.println();
