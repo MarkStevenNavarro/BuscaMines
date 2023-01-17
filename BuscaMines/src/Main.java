6b14ddedd62d2fc2c6252bac0d0e39ebd24a097b
Marc <marccorominas@inspladelestany.cat>
2023-01-17 15:55:15 +0100

    generar taulell

--- BuscaMines/src/Main.java
+++ BuscaMines/src/Main.java
@@ -1,5 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class Main {
+    Scanner llegir = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        generarTaulell();
+
+    }
+    public static void generarTaulell(){
+        Scanner llegir = new Scanner(System.in);
+        int alçada = Integer.parseInt(llegir.nextLine());
+        int base = Integer.parseInt(llegir.nextLine());
+        int[][] taulell = new int[alçada][base];
+        for(int i = 0; i<alçada; i++){
+            for(int j = 0; j<base; j++){
+                System.out.print(taulell[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println(taulell);
     }
 }
